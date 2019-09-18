4ca39a08b8fa844a9461ceb88ab21215bd5898c8
Алексей <aljasones@yandex.ru>
2019-09-19 01:11:57 +0300

    Вычисление расстояния, с известным расходом  и объемом топлива

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner consumption = new Scanner (System.in);
+        Scanner scanner = new Scanner (System.in);
         System.out.println("Enter average fuel consumption in 100 km");
-        double fuelConsuption = consumption.nextDouble(); //average fuel consumption
+        double fuelConsuption = scanner.nextDouble(); //average fuel consumption
         System.out.println("Your average fuel consumption of " + fuelConsuption + " liters on 100 km");
+        double fuelConsuptionOnOneKm = fuelConsuption / 100; //fuel consumption in 1 kilometer
 
-        Scanner volume = new Scanner(System.in);
         System.out.println("Enter the amount of fuel");
-        double fuelVolume = volume.nextDouble();//fuel remaining
+        double fuelVolume = scanner.nextDouble();//fuel remaining
         System.out.println("Your amount of fuel is " + fuelVolume + " liters");
 
-        double distance = fuelVolume / fuelConsuption;
+        double distance = fuelVolume / fuelConsuptionOnOneKm;
         int roundedDistance = (int) distance;
         System.out.println("The distance you can drive is " + roundedDistance + " km");
+        scanner.close();
 
     }
 }
